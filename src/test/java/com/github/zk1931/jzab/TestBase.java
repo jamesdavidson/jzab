8099cd990a78153e6b5ffacda222aabc1653d18d
James Davidson <jamesd3142@gmail.com>
2026-01-14 16:36:35 +0800

    Avoid putting colon in directory name

--- src/test/java/com/github/zk1931/jzab/TestBase.java
+++ src/test/java/com/github/zk1931/jzab/TestBase.java
@@ -169,7 +169,7 @@ public class TestBase {
   PersistentState makeInitialState(String serverId, int numTxnsInLog)
       throws IOException {
     PersistentState state =
-      new PersistentState(getDirectory().resolve(serverId).toFile());
+      new PersistentState(getDirectory().resolve(serverId.replace(':','_')).toFile());
 
     Log log = state.getLog();
     for (int i = 0; i < numTxnsInLog; ++i) {
